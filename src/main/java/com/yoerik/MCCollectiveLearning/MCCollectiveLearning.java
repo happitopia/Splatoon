fd51866c57fe0949ed13e635e7b15fb6a961f3fd
widakay <erik@yoerik.com>
2015-05-24 23:09:19 -0700

    actually added stuff

--- src/main/java/com/yoerik/MCCollectiveLearning/MCCollectiveLearning.java
+++ src/main/java/com/yoerik/MCCollectiveLearning/MCCollectiveLearning.java
@@ -1,4 +1,7 @@
 package com.yoerik.MCCollectiveLearning;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCCollectiveLearning extends JavaPlugin {
@@ -10,5 +13,20 @@ public final class MCCollectiveLearning extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		getLogger().info("onDisable has been invoked!");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("cl")) { // If the player typed /basic then do the following...
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+			} else {
+				Player player = (Player) sender;
+				sender.sendMessage("Usage: /cl help");
+			}
+			sender.sendMessage("HI");
+			return true;
+		}
+		return false;
 	}
 }
