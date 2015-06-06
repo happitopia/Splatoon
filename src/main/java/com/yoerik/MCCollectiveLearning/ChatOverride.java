243993308c6fb1c5b262b6e89a8476ba63ef3095
widdakay <erik@yoerik.com>
2015-06-06 11:13:00 -0700

    More cleanup on chat override

--- src/main/java/com/yoerik/MCCollectiveLearning/ChatOverride.java
+++ src/main/java/com/yoerik/MCCollectiveLearning/ChatOverride.java
@@ -1,49 +1,26 @@
 package com.yoerik.MCCollectiveLearning;
 
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ChatOverride {
-	private static HashMap<String, Boolean> allowedWords = new HashMap<String, Boolean>();
-	static {
-		allowedWords.put("tree", true);
-		allowedWords.put("block", true);
-		allowedWords.put("you", true);
-		allowedWords.put("me", true);
-		allowedWords.put("i", true);
-		allowedWords.put("water", true);
-		allowedWords.put("axe", true);
-		allowedWords.put("sword", true);
-		allowedWords.put("house", true);
-		allowedWords.put("bed", true);
-		allowedWords.put("food", true);
-		allowedWords.put("mob", true);
-		allowedWords.put("yes", true);
-		allowedWords.put("no", true);
-		allowedWords.put("dirt", true);
-		allowedWords.put("stone", true);
-		allowedWords.put("glass", true);
-		allowedWords.put("wood", true);
-		allowedWords.put("here", true);
-		allowedWords.put("there", true);
-		allowedWords.put("gem", true);
-		allowedWords.put("iron", true);
-		allowedWords.put("coal", true);
-		allowedWords.put("colonialism", true);
-		allowedWords.put("baseball", true);
-		allowedWords.put("thing", true);
-		allowedWords.put("animal", true);
-		allowedWords.put("farm", true);
-		allowedWords.put("day", true);
-		allowedWords.put("night", true);
-	}
+	private static final Set<String> allowedWords = new HashSet<String>(Arrays.asList(
+			new String[] {
+		"tree", "block", "you", "me", "i", "water", "axe", 
+		"sword", "house", "bed", "food", "mob", "yes", "no", 
+		"dirt", "stone", "glass", "wood", "here", "there", 
+		"gem", "iron", "coal", "colonialism", "baseball", 
+		"thing", "animal", "farm", "day", "night"
+		}));
 	
 	static String restrictMessage(String message) {
 		String finalMsg = "";
 		String[] splitted = message.split(" ");
 		for(int i = 0; i < splitted.length; i++) {
 			String word = splitted[i];
-			if (allowedWords.get(word.toLowerCase()) != null) {
+			if (allowedWords.contains(word.toLowerCase())) {
 				finalMsg += word + " ";
 			} else if (word.matches("-?\\d+(.\\d+)?")){
 				finalMsg += word + " ";
