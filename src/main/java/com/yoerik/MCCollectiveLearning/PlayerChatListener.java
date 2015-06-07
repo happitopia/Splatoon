8ab7025e14847fa58862feb6aa955d56dfb37424
widakay <erik@yoerik.com>
2015-06-06 17:52:02 -0700

    only restrict chat when ingame

--- src/main/java/com/yoerik/MCCollectiveLearning/PlayerChatListener.java
+++ src/main/java/com/yoerik/MCCollectiveLearning/PlayerChatListener.java
@@ -1,5 +1,6 @@
 package com.yoerik.MCCollectiveLearning;
 
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,24 +9,25 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatListener implements Listener {
 	public MCCollectiveLearning plugin;
+	
 	public PlayerChatListener(MCCollectiveLearning instance) {
 		this.plugin = instance;
 	}
 	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerChat(AsyncPlayerChatEvent chat) {
-
 		if (chat.isCancelled()) {
 			return;
 		} // no need to do anything.
-		
 		Player p = chat.getPlayer();
-		String message = ChatOverride.restrictMessage(chat.getMessage());
-		if (message == "") {
-			chat.setCancelled(true);
-			p.sendMessage("Please make sure that you are using the allowed vocabulary.");
-		} else {
-			chat.setMessage(message);
+		if (plugin.gameManager.isInGame(p)) {
+			String message = ChatOverride.restrictMessage(chat.getMessage());
+			if (message == "") {
+				chat.setCancelled(true);
+				p.sendMessage("Please make sure that you are using the allowed vocabulary.");
+			} else {
+				chat.setMessage(message);
+			}
 		}
 	}
 }
