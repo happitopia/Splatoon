486d59a3d6ca08d18222522e550df70fb9391f4e
widakay <erik@yoerik.com>
2015-06-15 18:07:42 -0700

    builds for me

---
+++ src/main/java/com/yoerik/SplatoonMinecraft/Team.java
@@ -0,0 +1,16 @@
+package com.yoerik.SplatoonMinecraft;
+
+
+import org.bukkit.Material;
+
+public class Team {
+	Material color;
+	
+	public Team(Material color) {
+		this.color = color;
+	}
+	
+	public Material getColor() {
+		return color;
+	}
+}
